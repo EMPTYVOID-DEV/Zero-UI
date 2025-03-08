49380d1ac45213678f0deb38ca5d0fd91513a4d6
EMPTYVOID-DEV <keskasaymen8@gmail.com>
2025-03-09 00:14:25 +0100

    1-removing css loader 2-removing types loader 3-fixing path reference

--- packages/cli/src/handlers/loadFiles.ts
+++ packages/cli/src/handlers/loadFiles.ts
@@ -7,21 +7,18 @@ import { logger } from "../utils/logger.js";
 export async function loadFiles(
   files: string[],
   parentDir: "components" | "icons",
-  componentsPath: string,
+  componentsPath: string
 ) {
-  const loadPromises = files.map((file) => {
-    const fileUrl = `${rawPath}/registry/${parentDir}/${file}`;
-    return githubFetch(fileUrl)
-      .then((content) => {
-        const filePath = path.join(workingDir, componentsPath, parentDir, file);
-        return writeFile(filePath, content);
-      })
-      .then(() => {
-        logger.success(`successfully loading and writing the file ${file}`);
-      })
-      .catch(() => {
-        logger.error(`There was an issue when writing this file ${file}`);
-      });
+  const loadPromises = files.map(async (file) => {
+    const fileUrl = `${rawPath}/${parentDir}/${file}`;
+    try {
+      const content = await githubFetch(fileUrl);
+      const filePath = path.join(workingDir, componentsPath, parentDir, file);
+      await writeFile(filePath, content);
+      logger.success(`successfully loading and writing the file ${file}`);
+    } catch {
+      logger.error(`There was an issue when writing this file ${file}`);
+    }
   });
   await Promise.allSettled(loadPromises);
 }
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,20 +6,17 @@ import { getRegistry } from "./handlers/getRegistry.js";
 import { cli } from "./handlers/cli.js";
 import { getDependencies } from "./handlers/getDependencies.js";
 import { loadFiles } from "./handlers/loadFiles.js";
-import { loadTheme, loadTypes } from "./handlers/loadUtility.js";
 import { installPackages } from "./handlers/installPackages.js";
 import { createLocalRegistry } from "./handlers/createLocalRegistry.js";
 
 async function main() {
   welcome();
   const registry = await getRegistry();
-  const { choices, componentsPath, isTypes } = await cli(registry);
+  const { choices, componentsPath } = await cli(registry);
   const { files, icons, packages } = getDependencies(registry, choices);
   await createLocalRegistry(choices, componentsPath, registry);
   await loadFiles(files, "components", componentsPath);
   await loadFiles(icons, "icons", componentsPath);
-  await loadTheme(componentsPath);
-  if (isTypes) await loadTypes(componentsPath);
   await installPackages(packages);
 }
 
